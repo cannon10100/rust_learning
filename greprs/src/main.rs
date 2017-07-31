0707fc194304af1f5d93865fec36d70e879d527f
cannon <cannon10100@gmail.com>
2017-07-31 17:16:59 -0500

    Finished through Ch. 12

---
+++ greprs/src/main.rs
@@ -0,0 +1,24 @@
+extern crate greprs;
+
+use std::env;
+use std::process;
+use std::io::prelude::*;
+
+use greprs::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        writeln!(std::io::stderr(), "Problem parsing arguments: {}", err)
+            .expect("Could not write to stderr");
+        process::exit(1);
+    });
+
+    if let Err(e) = greprs::run(config) {
+        writeln!(std::io::stderr(), "Application error: {}", e)
+            .expect("Could not write to stderr");
+
+        process::exit(1);
+    }
+}
